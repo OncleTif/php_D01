18b23ecae8f69e7a554dd0089d04eea1436f9ae8
tmanet <tmanet@student.42.fr>
2016-04-05 23:33:09 +0200

    ajout ex 11 12

---
+++ ex11/do_op_2.php
@@ -0,0 +1,50 @@
+#!/usr/bin/php
+<?php
+if ($argc == 2)
+{
+	if (strpos($argv[1], "+"))
+	{
+		$tab = explode("+", $argv[1])
+		if (is_numeric($tab[0]) && is_numeric($tab[1]))
+		echo $tab[0] + $tab[1]."/n";
+		else
+		echo "Syntax Error\n";
+	}
+	else if (strpos($argv[1], "-"))
+	{
+		$tab = explode("-", $argv[1])
+		if (is_numeric($tab[0]) && is_numeric($tab[1]))
+		echo $tab[0] - $tab[1]."/n";
+		else
+		echo "Syntax Error\n";
+	}
+	else if (strpos($argv[1], "*"))
+	{
+		$tab = explode("*", $argv[1])
+		if (is_numeric($tab[0]) && is_numeric($tab[1]))
+		echo $tab[0] * $tab[1]."/n";
+		else
+		echo "Syntax Error\n";
+	}
+	else if (strpos($argv[1], "/"))
+	{
+		$tab = explode("/", $argv[1])
+		if (is_numeric($tab[0]) && is_numeric($tab[1]))
+		echo $tab[0] / $tab[1]."/n";
+		else
+		echo "Syntax Error\n";
+	}
+	else if (strpos($argv[1], "%"))
+	{
+		$tab = explode("%", $argv[1])
+		if (is_numeric($tab[0]) && is_numeric($tab[1]))
+		echo $tab[0] % $tab[1]."/n";
+		else
+		echo "Syntax Error\n";
+	}
+	else
+	echo "Syntax Error\n";
+}
+else
+echo "Incorrect Parameters\n";
+?>
