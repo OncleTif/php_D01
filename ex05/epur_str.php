9f09323297a6160b12165a720d9245812da31a4f
tmanet <tmanet@student.42.fr>
2016-04-06 18:04:59 +0200

    protection du 0 et correction du first elem

--- ex05/epur_str.php
+++ ex05/epur_str.php
@@ -1,12 +1,20 @@
 #!/usr/bin/php
 <?php
+function fct_eval_array($string)
+{
+	if ($string === "")
+		return(0);
+	return(1);
+	}
 if ($argc == 2)
 {
-$tab = array_filter(explode(" ",$argv[1]));
+$tab = array_filter(explode(" ",$argv[1]), "fct_eval_array");
 foreach ($tab as $index => $word)
 {
-	if ($index)
+	if (isset($not_first))
 		echo " ";
+	else
+		$not_first = 1;
 	echo $word;
 	}
 echo "\n";
