9c804344c966a85d52f5ff2ebcc8fbc5241f3cba
tmanet <tmanet@student.42.fr>
2016-04-06 14:52:11 +0200

    correction de do op 2

--- ex11/do_op_2.php
+++ ex11/do_op_2.php
@@ -4,47 +4,47 @@ if ($argc == 2)
 {
 	if (strpos($argv[1], "+"))
 	{
-		$tab = explode("+", $argv[1])
+		$tab = explode("+", $argv[1]);
 		if (is_numeric($tab[0]) && is_numeric($tab[1]))
-		echo $tab[0] + $tab[1]."/n";
+			echo $tab[0] + $tab[1]."\n";
 		else
-		echo "Syntax Error\n";
-	}
-	else if (strpos($argv[1], "-"))
-	{
-		$tab = explode("-", $argv[1])
-		if (is_numeric($tab[0]) && is_numeric($tab[1]))
-		echo $tab[0] - $tab[1]."/n";
-		else
-		echo "Syntax Error\n";
-	}
-	else if (strpos($argv[1], "*"))
-	{
-		$tab = explode("*", $argv[1])
-		if (is_numeric($tab[0]) && is_numeric($tab[1]))
-		echo $tab[0] * $tab[1]."/n";
-		else
-		echo "Syntax Error\n";
-	}
-	else if (strpos($argv[1], "/"))
-	{
-		$tab = explode("/", $argv[1])
-		if (is_numeric($tab[0]) && is_numeric($tab[1]))
-		echo $tab[0] / $tab[1]."/n";
-		else
-		echo "Syntax Error\n";
-	}
-	else if (strpos($argv[1], "%"))
-	{
-		$tab = explode("%", $argv[1])
-		if (is_numeric($tab[0]) && is_numeric($tab[1]))
-		echo $tab[0] % $tab[1]."/n";
-		else
-		echo "Syntax Error\n";
-	}
-	else
-	echo "Syntax Error\n";
-}
-else
-echo "Incorrect Parameters\n";
+			echo "Syntax Error\n";
+			}
+			else if (strpos($argv[1], "*"))
+			{
+				$tab = explode("*", $argv[1]);
+				if (is_numeric($tab[0]) && is_numeric($tab[1]))
+					echo $tab[0] * $tab[1]."\n";
+				else
+					echo "Syntax Error\n";
+					}
+					else if (strpos($argv[1], "/"))
+					{
+						$tab = explode("/", $argv[1]);
+						if (is_numeric($tab[0]) && is_numeric($tab[1]))
+							echo $tab[0] / $tab[1]."\n";
+						else
+							echo "Syntax Error\n";
+							}
+							else if (strpos($argv[1], "%"))
+							{
+								$tab = explode("%", $argv[1]);
+								if (is_numeric($tab[0]) && is_numeric($tab[1]))
+									echo $tab[0] % $tab[1]."\n";
+								else
+									echo "Syntax Error\n";
+									}
+									else if (strpos($argv[1], "-"))
+									{
+										$tab = explode("-", $argv[1]);
+										if (is_numeric($tab[0]) && is_numeric($tab[1]))
+											echo $tab[0] - $tab[1]."\n";
+										else
+											echo "Syntax Error\n";
+											}
+											else
+												echo "Syntax Error\n";
+												}
+												else
+													echo "Incorrect Parameters\n";
 ?>
