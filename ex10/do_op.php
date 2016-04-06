897cee128500a1d4c6424efcdc93effd5656621d
tmanet <tmanet@student.42.fr>
2016-04-06 18:26:53 +0200

    suppression des verifs d'erreures

--- ex10/do_op.php
+++ ex10/do_op.php
@@ -1,6 +1,6 @@
 #!/usr/bin/php
 <?php
-if ($argc == 4 && is_numeric($argv[1]) && is_numeric($argv[3]))
+if ($argc == 4)
 {
 	if (trim($argv[2]) == "+")
 		echo $argv[1] + $argv[3]."\n";
@@ -12,8 +12,6 @@ if ($argc == 4 && is_numeric($argv[1]) && is_numeric($argv[3]))
 		echo $argv[1] / $argv[3]."\n";
 	else if (trim($argv[2]) == "%")
 		echo $argv[1] % $argv[3]."\n";
-	else
-	echo "Incorrect Parameters\n";
 }
 else
 	echo "Incorrect Parameters\n";
