bf3f3a666af37dab4304c3c40958eff8adccde36
tmanet <tmanet@student.42.fr>
2016-04-06 18:14:42 +0200

    protection contre les 0

--- ex06/ssap.php
+++ ex06/ssap.php
@@ -1,11 +1,17 @@
 #!/usr/bin/php
 <?php
+function fct_eval_array($string)
+{
+	if ($string === "")
+		return(0);
+	return(1);
+	}
 $i = 1;
 if ($argc > 1)
 {
 while ($i < $argc)
 {
-$tab[$i] = array_filter(explode(" ",$argv[$i]));
+$tab[$i] = array_filter(explode(" ",$argv[$i]), "fct_eval_array");
 $i++;
 }
 $merge = array();
--- ex07/rostring.php
+++ ex07/rostring.php
@@ -1,8 +1,14 @@
 #!/usr/bin/php
 <?php
+function fct_eval_array($string)
+{
+	if ($string === "")
+		return(0);
+	return(1);
+	}
 if ($argc > 1)
 {
-$tab = array_filter(explode(" ", $argv[1]));
+$tab = array_filter(explode(" ", $argv[1]), "fct_eval_array");
 $first = array_shift($tab);
 foreach ($tab as $elem)
 	echo $elem." ";
