d17275253595bd97f867a33855d9da700bc1ef01
tmanet <tmanet@student.42.fr>
2016-04-06 15:12:48 +0200

    search ok

--- ex12/search_it!.php
+++ ex12/search_it!.php
@@ -2,7 +2,16 @@
 <?php
 if ($argc > 2)
 {
-	
-	
+$ret = NULL;
+$i = 2;
+while ($i < $argc)
+{
+$param = explode(":", $argv[$i]);
+	if (sizeof($param) == 2 && $argv[1] === $param[0])
+		$ret = $param[1];
+$i++;
+}
+if ($ret !== NULL)
+echo $ret."\n";
 }
 ?>
